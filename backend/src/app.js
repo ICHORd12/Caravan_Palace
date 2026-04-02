db777da82bda84efa377b8c9f2f1f3de7b8eb96f
Ege Büyükacaroğlu <egebuyukacaroglu@gmail.com>
2026-04-03 01:48:10 +0300

    Some tests reverted to dev

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const cors = require("cors");//ADD TRİAL
 const routes = require("./routes");
 const notFoundMiddleware = require("./middlewares/notFoundMiddleware");
 const errorMiddleware = require("./middlewares/errorMiddleware");
 
-const app = express(); // ADD TRİAL
+const app = express();
 
-app.use(cors());//ADD TRİAL
 app.use(express.json());
 app.use("/api/v1", routes);
 
