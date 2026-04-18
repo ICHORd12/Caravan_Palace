dbbec4d17c28d12357b2691d05ff7a1133d7aa8b
Fatih Sarıoğlu <ftihsrglu@gmail.com>
2026-04-18 13:15:00 +0300

    merge user and address routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,13 +3,11 @@ const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
 const productRoutes = require("./productRoutes");
 const cartRoutes = require("./cartRoutes");
-const addressRoutes = require("./addressRoutes");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
-router.use("/users/me/addresses", addressRoutes);
 router.use("/products", productRoutes);
 router.use("/cart", cartRoutes);
 
