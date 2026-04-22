da71c61adc2882e467a02366f80d4e90f7b4e3ec
Fatih Sarıoğlu <ftihsrglu@gmail.com>
2026-04-22 15:13:37 +0300

    add the endpoint and documentation for it

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const addressRoutes = require("./addressRoutes");
+const orderRoutes = require("./orderRoutes");
 
 const router = express.Router();
 
 router.get("/me", authMiddleware, userController.me);
 router.use("/me/addresses", addressRoutes);
+router.use("/me/orders", orderRoutes);
 
 module.exports = router;
