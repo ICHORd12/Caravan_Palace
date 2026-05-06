cd2263affd713ed7fa73d07d8bb3cac7e19f2969
Ege Büyükacaroğlu <egebuyukacaroglu@gmail.com>
2026-05-06 15:31:13 +0300

    ID return added to shopping cart

--- frontend/constants/API.ts
+++ frontend/constants/API.ts
@@ -27,7 +27,7 @@ export const UPDATE_PROFILE_ENDPOINT = `${base}/users/me`;
 export const ADDRESSES_ENDPOINT = `${base}/users/me/addresses`;
 export const GET_ORDERS_END_POINT = `${base}/users/me/orders`;
 export const GET_ORDER_DETAILS_END_POINT = `${base}/users/me/orders`;
-export const INVOICE_API_END_POINT = '/invoices';
+export const INVOICE_API_END_POINT = `${base}/invoices`;
 export const REVIEWS_ENDPOINT = `${base}/reviews`
 
 
